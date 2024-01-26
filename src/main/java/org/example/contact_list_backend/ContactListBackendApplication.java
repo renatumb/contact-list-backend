bfec5dc588446974e3aeabf0a3ba21d66547cdde
renatu <renatu_mb@yahoo.com.br>
2024-01-26 00:16:01 -0800

    Initial Commit

---
+++ src/main/java/org/example/contact_list_backend/ContactListBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.contact_list_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactListBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/contact_list_backend/ContactListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.contact_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
